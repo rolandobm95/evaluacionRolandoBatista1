8fb29b4d1bb7713441aa2b08d2520f65f5cfa11e
rolando <rolandobm95@gmail.com>
2023-11-02 18:10:48 -0500

    Version 2 Se adiciona logica a la funcion main

--- src/Main.java
+++ src/Main.java
@@ -10,22 +10,23 @@ public class Main {
         int fila = 0;
         int columna = 0;
 
-
+        //Validacion y entrada de datos
+        do {
             System.out.print("Ingrese el numero entero que desea posicionar en la matriz: ");
             if (scanner.hasNextInt()) {
                 numero = scanner.nextInt();
-
+                break;
             } else {
                 System.out.println("Entrada no es un valor válido.");
                 scanner.next(); // Limpiar el búfer de entrada para evitar un bucle infinito
             }
-
-
+        }while(true);
+        do {
             System.out.print("Ingrese la fila donde desea posicionar el numero: ");
             if (scanner.hasNextInt()) {
                 fila = scanner.nextInt();
                 if (fila >= 1 && fila <= 5){
-
+                    break;
                 } else {
                     System.out.println("El numero de fila debe estar entre 1 y 5");
                 }
@@ -33,13 +34,13 @@ public class Main {
                 System.out.println("Entrada no es un valor válido.");
                 scanner.next(); // Limpiar el búfer de entrada para evitar un bucle infinito
             }
-
-
+        }while(true);
+        do {
             System.out.print("Ingrese la columna donde desea posicionar el numero: ");
             if (scanner.hasNextInt()) {
                 columna = scanner.nextInt();
                 if (columna >= 1 && columna <= 5){
-
+                    break;
                 } else {
                     System.out.println("El numero de columna debe estar entre 1 y 5");
                 }
@@ -47,7 +48,7 @@ public class Main {
                 System.out.println("Entrada no es un valor válido.");
                 scanner.next(); // Limpiar el búfer de entrada para evitar un bucle infinito
             }
-
+        }while(true);
 
         //Se almacena la matriz resultante para visualizarla
         int[][]M1 = matriz.GenararMatriz(numero, fila, columna);
